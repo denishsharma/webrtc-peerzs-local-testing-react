1e5cbe5aee4480f5a452a9693535693f5e131fff
Denish Sharma <denishsharma@ternaengg.ac.in>
2024-06-08 18:08:18 +0530

    refactor: :wastebasket: clean template files and update project description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-import { WelcomeFragment } from "~/modules/welcome/welcome-fragment";
-
 export const Route = createFileRoute("/")({
     component: HomePage,
 });
 
 function HomePage() {
     return (
-        <WelcomeFragment />
+        <div>
+            Peerzs WebRTC Testing
+        </div>
     );
 }
